7b8f7e41681110b3a434ea5431a6f2f97ca7d565
User <nazarovtolya2@gmail.com>
2023-04-16 21:18:58 +0500

    Выполнение ДЗ

--- src/Truck.java
+++ src/Truck.java
@@ -2,6 +2,9 @@ public class Truck extends Transport implements TransportService{
     public Truck(String modelName, int wheelsCount) {
         super(modelName, wheelsCount);
     }
+    public void updateTyre() {
+        System.out.println("Меняем покрышку у грузовика");
+    }
     public void checkEngine() {
         System.out.println("Проверяем двигатель грузовика");
     }
